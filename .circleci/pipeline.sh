c11f1ce549a3f51903590ae4ca2ea58e22485de5
elijah0kello <elijahokello90@gmail.com>
2024-08-25 23:46:11 +0200

    ci: added bash script to define logic

--- .circleci/pipeline.sh
+++ .circleci/pipeline.sh
@@ -1,15 +1,14 @@
 #! /bin/sh
 
-function hello_world() {
-  echo "Hello, World!"
-}
-
-function deploy() {
-  echo "Deploying application..."
-  # Add your deployment commands here
-}
-
-function cleanup() {
-  echo "Cleaning up..."
-  # Add your cleanup commands here
+defaults_configure_nvm() {
+  touch "$HOME/.profile"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  local node_version
+  node_version=$(cat .nvmrc)
+  echo "Installing Node version: $node_version"
+  nvm install "$node_version"
+  nvm alias default "$node_version"
+  nvm use "$node_version"
 }
