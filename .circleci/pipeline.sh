ad40cf3ce4e2e9cb7e56e9ce94a517ae96da6531
elijah0kello <elijahokello90@gmail.com>
2024-08-25 23:25:24 +0200

    ci: added bash script to define logic

--- .circleci/pipeline.sh
+++ .circleci/pipeline.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
 
-echo "Pipeline File"
+function hello_world() {
+  echo "Hello, World!"
+}
+
+function deploy() {
+  echo "Deploying application..."
+  # Add your deployment commands here
+}
+
+function cleanup() {
+  echo "Cleaning up..."
+  # Add your cleanup commands here
+}
