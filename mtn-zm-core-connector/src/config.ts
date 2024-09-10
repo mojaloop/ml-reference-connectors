93f09ca78216622c735e7d655a73f7b06a01a6ed
Mcwayz <mcwayzj@gmail.com>
2024-09-10 10:56:35 +0200

    feat: added auth and kyc

---
+++ mtn-zm-core-connector/src/config.ts
@@ -0,0 +1,66 @@
+import "dotenv/config";
+import Convict from 'convict';
+import { TSDKSchemeAdapterConfig } from './domain/SDKClient';
+import { TMTNConfig } from "./domain/CBSClient";
+
+interface IConfigSchema {
+    server: {
+        SDK_SERVER_HOST: string;
+        SDK_SERVER_PORT: number;
+        DFSP_SERVER_HOST: string;
+        DFSP_SERVER_PORT: number;
+    };
+    sdkSchemeAdapter: TSDKSchemeAdapterConfig;
+    mtn:TMTNConfig;
+}
+
+const config = Convict<IConfigSchema>({
+    server: {
+        SDK_SERVER_HOST: {
+            doc: 'SDK Server host',
+            format: String,
+            default: null, // required
+            env: 'SDK_SERVER_HOST',
+        },
+        SDK_SERVER_PORT: {
+            doc: 'SDK Server port',
+            format: Number,
+            default: 3000, // optional
+            env: 'SDK_SERVER_PORT',
+        },
+        DFSP_SERVER_HOST: {
+            doc: 'DFSP operations app Server host',
+            format: String,
+            default: null, // required
+            env: 'DFSP_SERVER_HOST',
+        },
+        DFSP_SERVER_PORT: {
+            doc: 'dfsp operations app Server port',
+            format: Number,
+            default: null, // required
+            env: 'DFSP_SERVER_PORT',
+        },
+    },
+    sdkSchemeAdapter: {
+        SDK_BASE_URL: {
+            doc: 'SDK Scheme Adapter Base URL',
+            format: String,
+            default: null, // required
+            env: 'SDK_BASE_URL',
+        },
+    },
+    mtn:{
+        CBS_NAME: {
+            doc: 'Name of the DFSP',
+            format: String,
+            default: null, // required
+            env: 'CBS_NAME',
+        },
+    }
+});
+
+config.validate({ allowed: 'strict' });
+
+export type TConfig = Convict.Config<IConfigSchema>;
+
+export default config;
