5276cf5fc64c28083ed9fb7505ae025c2ed5df6e
elijah0kello <elijahokello90@gmail.com>
2024-09-21 15:36:08 +0300

    fix: fixed failing tests

--- tnm-mw-core-connector/test/unit/domain/coreConnectorAgg.test.ts
+++ tnm-mw-core-connector/test/unit/domain/coreConnectorAgg.test.ts
@@ -151,7 +151,7 @@ describe('CoreConnectorAggregate Tests -->', () => {
                 }
             })
 
-            jest.spyOn(tnmClient, "makepayment");
+            jest.spyOn(tnmClient, "sendMoney");
 
             const patchNotificationRequest: TtransferPatchNotificationRequest = transferPatchNotificationRequestDto("COMPLETED", idType, MSISDN_NO, "500");
             const res = await ccAggregate.updateTransfer(patchNotificationRequest, "ljzowczj");
@@ -198,22 +198,19 @@ describe('CoreConnectorAggregate Tests -->', () => {
                     "expires_at": "2023-07-13 10:56:45"
                 }
             });
-            tnmClient.sendMoney = jest.fn().mockResolvedValue(
+            tnmClient.collectMoney = jest.fn().mockResolvedValue(
                 {
-                    "message": "Completed successfully",
+                    "message": "Request accepted and processing",
                     "errors": [],
                     "trace": [],
-                    "data": {
-                        "transaction_id": "ljzowczj",
-                        "receipt_number": "AGC00B5MCA"
-                    }
-                }
+                    "data": []
+                  }
             );
-            jest.spyOn(tnmClient, "makepayment");
+            jest.spyOn(tnmClient, "collectMoney");
             const updateSendMoneyReqBody = tnmUpdateSendMoneyRequestDto(MSISDN_NO, "1000");
             const res = await ccAggregate.updateSendMoney(updateSendMoneyReqBody, "ljzowczj");
             logger.info("Response ", res);
-            expect(tnmClient.sendMoney).toHaveBeenCalled();
+            expect(tnmClient.collectMoney).toHaveBeenCalled();
         });
 
         test("PUT /callback: should call mojaloop connector with acceptQuote: true if payment was successful", async () => {
