f83eb218427464295b53da80a9c9e3c9741eabae
Elijah Okello <elijahokello90@gmail.com>
2025-01-28 07:07:16 +0000

    fix: fixed failing tests

--- airtel-zm-core-connector/test/fixtures.ts
+++ airtel-zm-core-connector/test/fixtures.ts
@@ -1,94 +1,8 @@
-import { TSDKOutboundTransferRequest, TSDKOutboundTransferResponse, TUpdateTransferDeps } from '../src/domain/SDKClient';
-import { TAirtelSendMoneyRequest, TAirtelUpdateSendMoneyRequest, TCallbackRequest, TFineractGetAccountResponse, TFineractTransactionResponse } from '../src/domain/CBSClient';
+import { TSDKOutboundTransferRequest, TSDKOutboundTransferResponse, } from '../src/domain/SDKClient';
+import { TAirtelSendMoneyRequest, TAirtelUpdateSendMoneyRequest, TCallbackRequest} from '../src/domain/CBSClient';
 import * as crypto from 'node:crypto';
 import { TtransferPatchNotificationRequest, TQuoteRequest, TtransferRequest, THttpResponse } from 'src/domain/interfaces/types';
 import { components } from '@mojaloop/api-snippets/lib/sdk-scheme-adapter/v2_1_0/outbound/openapi';
-
-type TransferAcceptInputDto = {
-  fineractAccountId?: number;
-  totalAmount?: number;
-  sdkTransferId?: number;
-};
-
-
-export const transferAcceptDto = ({
-  fineractAccountId = 1,
-  totalAmount = 123.45,
-  sdkTransferId = 999,
-}: TransferAcceptInputDto = {}): TUpdateTransferDeps =>
-  ({
-    fineractTransaction: {
-      fineractAccountId,
-      totalAmount,
-      routingCode: 'routingCode',
-      receiptNumber: 'receiptNumber',
-      bankNumber: 'bankNumber',
-    },
-    sdkTransferId,
-  }) as const;
-
-// todo: make it configurable, add all required fields
-export const fineractGetAccountResponseDto = (): Partial<TFineractGetAccountResponse> =>
-  ({
-    id: 'id',
-    accountNo: 'accountNo',
-    clientId: 123,
-    clientName: 'clientName',
-  }) as const;
-
-// todo: make it configurable,
-export const fineractTransactionResponseDto = (): TFineractTransactionResponse =>
-  ({
-    officeId: 1,
-    clientId: 2,
-    savingsId: 3,
-    resourceId: 4,
-    changes: {
-      accountNumber: 'accountNumber',
-      routingCode: 'routingCode',
-      receiptNumber: 'receiptNumber',
-      bankNumber: 'bankNumber',
-    },
-  }) as const;
-
-export const fineractLookUpPartyResponseDto = () =>
-  ({
-    displayName: 'Dove Love',
-    firstname: 'Dove',
-    lastname: 'Love',
-  }) as const;
-
-export const fineractVerifyBeneficiaryResponseDto = () =>
-  ({
-    currency: 'UGX',
-    amount: '100',
-    quoteId: crypto.randomUUID(),
-    transactionId: crypto.randomUUID(),
-  }) as const;
-
-export const fineractGetAccountIdResponseDto = () => ({
-  accountId: 1,
-});
-
-export const fineractReceiveTransferResponseDto = () => true;
-
-export const fineractGetSavingsAccountResponseDto = (
-  credit: boolean,
-  debit: boolean,
-  balance: number,
-  active: boolean,
-) => ({
-  status: {
-    active: active,
-  },
-  subStatus: {
-    blockCredit: credit,
-    blockDebit: debit,
-  },
-  summary: {
-    availableBalance: balance,
-  },
-});
 
 export const sdkInitiateTransferResponseDto = (idValue: string, currentState: components["schemas"]["transferStatus"]): THttpResponse<TSDKOutboundTransferResponse> => ({
   statusCode: 200,
@@ -196,9 +110,6 @@ export const sdkInitiateTransferResponseDto = (idValue: string, currentState: co
     }
   }
 });
-
-export const fineractCalculateWithdrawQuoteResponseDto = (feeAmount: number) => feeAmount;
-
 
 export const transferPatchNotificationRequestDto = (currentState: string, partyIdType: string, partyIdentifier: string, amount: string): TtransferPatchNotificationRequest => ({
   //@ts-expect-error currentState var to of type
@@ -349,12 +260,24 @@ export const transferRequestDto = (idType: string, idValue: string, amount: stri
   from: {
     //@ts-expect-error partyIdType var not of type IdType
     idType: idType,
-    idValue: idValue
+    idValue: idValue,
+    extensionList: [
+      {
+        "key": "testkey",
+        "value": "TestVal"
+      }
+    ]
   },
   to: {
     //@ts-expect-error partyIdType var not of type IdType
     idType: idType,
-    idValue: idValue
+    idValue: idValue,
+    extensionList: [
+      {
+        "key": "testkey",
+        "value": "TestVal"
+      }
+    ]
   },
   ilpPacket: {
     data: {
