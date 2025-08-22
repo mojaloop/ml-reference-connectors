a497045eec988915ebfa3168343757dcca522da7
elijah0kello <elijahokello90@gmail.com>
2025-08-22 15:15:45 +0200

    fix: pass transferErrorResponse correctly

--- src/domain/utils.ts
+++ src/domain/utils.ts
@@ -1,5 +1,5 @@
 import { TtransferErrorResponse } from './SDKClient';
 
-export function isAxiosLikeError(error: unknown): error is { response: TtransferErrorResponse } {
+export function isAxiosLikeError(error: unknown): error is { response: {data: TtransferErrorResponse} } {
     return typeof error === 'object' && error !== null && 'response' in error;
 }
